2400fc13da9c20f600556b8ac6f1670b4a0415fc
Chris Brite <chrismbrite@gmail.com>
2022-04-11 05:07:24 -0700

    Full Game working

--- src/com/skilldistillery/blackjack/BlackJackApplication.java
+++ src/com/skilldistillery/blackjack/BlackJackApplication.java
@@ -23,18 +23,19 @@ public class BlackJackApplication {
 		Hand dealer = new Hand();
 		Deck deck = new Deck();
 		deck.shuffle();
+		boolean bust = false;
 
 		// Deals user cards
 		user.addCard(deck.dealCard());
 		user.addCard(deck.dealCard());
 		// Sysout to show user cards
-		System.out.println("Player hand" + user.toString());
+		System.out.println("Player hand" + user.toString() + " = " + user.getHandValue());
 
 		// Deals dealer cards
 		dealer.addCard(deck.dealCard());
 		dealer.addCard(deck.dealCard());
 		// Sysout to show dealer cards
-		System.out.println("Dealer hand" + dealer.toString());
+		System.out.println("Dealer hand" + dealer.toString() +  " = " + dealer.getHandValue());
 		String input;
 		
 		do {
@@ -42,11 +43,16 @@ public class BlackJackApplication {
 			System.out.println("Enter Y for another card or N to stand.");
 			input = sc.nextLine();
 			// If player chooses to hit
+			if(user.getHandValue() == 21) {
+				System.out.println(" BLACKJACK! You have won!");
+				bust = true;
+			}
 			if (input.equalsIgnoreCase("Y")) {
 				user.addCard(deck.dealCard());
-				System.out.println("Player hand" + user.toString());
+				System.out.println("Player drew another card to total " + user.getHandValue() + " " + user.toString());
 				if (user.getHandValue() > 21) {
 					System.out.println(user.getHandValue() + " Player Bust!");
+					bust = true;
 				}
 			}
 			// If player chooses to stay
@@ -54,9 +60,9 @@ public class BlackJackApplication {
 				System.out.println("Player stays with total of " + user.getHandValue());
 			}
 			// Dealer logic
-			if (dealer.getHandValue() < 17) {
+			if (user.getHandValue() < 22 && dealer.getHandValue() < 17) {
 				dealer.addCard(deck.dealCard());
-				System.out.println("Dealer drew another card to total " + dealer.getHandValue());
+				System.out.println("Dealer drew another card to total " + dealer.getHandValue() + " " + dealer.toString());
 
 			}
 			if (dealer.getHandValue() > 16) {
@@ -64,14 +70,21 @@ public class BlackJackApplication {
 
 			}
 			if (dealer.getHandValue() > 21) {
-				System.out.println(dealer.getHandValue() + " Dealer bust, player wins!");
+				System.out.println(dealer.getHandValue() + " Dealer bust!");
+				bust = true;
 			}
-		} while (dealer.getHandValue() < 21 && user.getHandValue() < 21 || !input.equalsIgnoreCase("N"));
+		} while (bust = false);
 		// Game winner
-		if (dealer.getHandValue() > user.getHandValue()) {
+		if (dealer.getHandValue() < 22 && dealer.getHandValue() > user.getHandValue()) {
 			System.out.println("Dealer wins this time!");
 		}
-		if (user.getHandValue() > dealer.getHandValue()) {
+		if (user.getHandValue() > 22 && dealer.getHandValue() < 22) {
+			System.out.println("Dealer wins this time!");
+		}
+		if (user.getHandValue() < 22 && user.getHandValue() > dealer.getHandValue()) {
+			System.out.println("Player wins this time!");
+		}
+		if (user.getHandValue() < 22 && dealer.getHandValue() > 22) {
 			System.out.println("Player wins this time!");
 		}
 		if (dealer.getHandValue() == user.getHandValue()) {
