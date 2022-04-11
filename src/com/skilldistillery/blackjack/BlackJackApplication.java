65ee05c8006d33af220fd5d61a46e13fee3a2bb2
Chris Brite <chrismbrite@gmail.com>
2022-04-11 04:19:04 -0700

    Dealer rules

--- src/com/skilldistillery/blackjack/BlackJackApplication.java
+++ src/com/skilldistillery/blackjack/BlackJackApplication.java
@@ -39,11 +39,34 @@ public class BlackJackApplication {
 		System.out.println("Would you like to hit? Y/N");
 		System.out.println("Enter Y for another card or N to stand.");
 		String input = sc.nextLine();
+		// If player chooses to hit
 		if (input.equalsIgnoreCase("Y")) {
 			user.addCard(deck.dealCard());
 			System.out.println("Player hand" + user.toString());
+			if (user.getHandValue() > 21) {
+				System.out.println(user.getHandValue() + " Player Bust!");
+			}
+		}
+		// If player chooses to stay
+		if (input.equalsIgnoreCase("N")) {
+			System.out.println("Player stays with total of " + user.getHandValue());
+		}
+
+		// Dealer logic
+		if (dealer.getHandValue() < 17) {
+			dealer.addCard(deck.dealCard());
+			System.out.println("Dealer drew another card to total " + dealer.toString());
 
 		}
+		if (dealer.getHandValue() > 16) {
+			System.out.println("Dealer stands at " + dealer.toString());
+			
+		}
+		if (dealer.getHandValue() > 21) {
+			System.out.println(dealer.getHandValue() + " Dealer bust, player wins!");
+		}
+		
+		//Game winner
 
 	}
 }
